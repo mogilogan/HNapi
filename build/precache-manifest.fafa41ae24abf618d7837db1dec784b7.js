62a083a36a4a81dd97e51b10f9268a393e13b94d
mogilogan <mogiegan@gamil.com>
2022-03-10 01:40:22 +0530

    Second commit

---
+++ build/precache-manifest.fafa41ae24abf618d7837db1dec784b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6272a91b8a90eab4c30",
+    "url": "/HNapi/static/css/main.dd71afc7.chunk.css"
+  },
+  {
+    "revision": "a6272a91b8a90eab4c30",
+    "url": "/HNapi/static/js/main.9e512dcf.chunk.js"
+  },
+  {
+    "revision": "315f65ac7e2511212212",
+    "url": "/HNapi/static/js/runtime~main.7a665dbc.js"
+  },
+  {
+    "revision": "e23135bfe929451c2007",
+    "url": "/HNapi/static/js/2.c47328ca.chunk.js"
+  },
+  {
+    "revision": "0b5dce7610298604feee",
+    "url": "/HNapi/static/js/3.d91048eb.chunk.js"
+  },
+  {
+    "revision": "7bed11a827ee05123a123c13d399ef63",
+    "url": "/HNapi/index.html"
+  }
+];
